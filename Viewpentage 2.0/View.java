dfccea496c2eaa827a249e5db3a77a2a031699e0
alphaaffe <46808983+alphaaffe@users.noreply.github.com>
2019-02-01 06:17:40 +0100

    Add files via upload

--- Viewpentage 2.0/View.java
+++ Viewpentage 2.0/View.java
@@ -1,4 +1,3 @@
-
 import java.awt.event.*;
 import java.awt. * ;
 import javax.swing.*;
@@ -14,6 +13,12 @@ public class View  extends JFrame
 {
     JPanel myJPanel                 = new JPanel();
     JPanel SpielfeldJPanel          = new JPanel();
+    JPanel FeldAJPanel              = new JPanel();
+    JPanel FeldBJPanel              = new JPanel();
+    JPanel FeldCJPanel              = new JPanel();
+    JPanel FeldDJPanel              = new JPanel();
+    
+    
     BorderLayout myBorderLayout     = new BorderLayout();
     
     JTextField spieler1TextField    = new JTextField();
@@ -35,13 +40,20 @@ public class View  extends JFrame
 
 
     public View() {
-        //Controller myController= new Controller(this);
+        SController PentagoController= new SController(this);
 
         getContentPane().setLayout(myBorderLayout);
         myJPanel.setLayout(null);
         SpielfeldJPanel.setBounds(new Rectangle(150, 150, 700, 700));
         SpielfeldJPanel.setBackground(Color.black);
-        
+        FeldAJPanel.setBounds(new Rectangle(180, 180, 295, 295));
+        FeldAJPanel.setBackground(Color.red);
+        FeldBJPanel.setBounds(new Rectangle(505, 180, 295, 295));
+        FeldBJPanel.setBackground(Color.blue);
+        FeldCJPanel.setBounds(new Rectangle(180, 505, 295, 295));
+        FeldCJPanel.setBackground(Color.green);
+        FeldDJPanel.setBounds(new Rectangle(505, 505, 295, 295));
+        FeldDJPanel.setBackground(Color.orange);
         
         spieler1TextField.setBounds(new Rectangle(400, 70, 75, 40));//(x,y,länge,breite)
         spieler2TextField.setBounds(new Rectangle(525, 70, 75, 40));
@@ -54,56 +66,62 @@ public class View  extends JFrame
         DrehenARechtsButton.setText("Drehen");
         DrehenARechtsButton.setBounds(new Rectangle(150, 30, 100, 100));
         DrehenARechtsButton.setActionCommand("DrehenARechts");
-        //DrehenARechtsButton.addActionListener(myController);
+        DrehenARechtsButton.addActionListener(PentagoController);
 
         
         DrehenALinksButton.setText("Drehen");
         DrehenALinksButton.setBounds(new Rectangle(30, 150, 100, 100));
         DrehenALinksButton.setActionCommand("DrehenALinks");
-        //DrehenALinksButton.addActionListener(myController);
+        DrehenALinksButton.addActionListener(PentagoController);
 
         
         DrehenBRechtsButton.setText("Drehen");
         DrehenBRechtsButton.setBounds(new Rectangle(870, 150, 100, 100));
         DrehenBRechtsButton.setActionCommand("DrehenBRechts");
-        //DrehenBRechtsButton.addActionListener(myController);
+        DrehenBRechtsButton.addActionListener(PentagoController);
         
         
         DrehenBLinksButton.setText("Drehen");
         DrehenBLinksButton.setBounds(new Rectangle(750, 30, 100, 100));
         DrehenBLinksButton.setActionCommand("DrehenBLinks");
-        //DrehenBLinksButton.addActionListener(myController);
+        DrehenBLinksButton.addActionListener(PentagoController);
 
         
         DrehenCRechtsButton.setText("Drehen");
         DrehenCRechtsButton.setBounds(new Rectangle(30, 750, 100, 100));
         DrehenCRechtsButton.setActionCommand("DrehenCRechts");
-        //DrehenCRechtsButton.addActionListener(myController);
+        DrehenCRechtsButton.addActionListener(PentagoController);
 
         
         DrehenCLinksButton.setText("Drehen");
         DrehenCLinksButton.setBounds(new Rectangle(150, 870, 100, 100));
         DrehenCLinksButton.setActionCommand("DrehenCLinks");
-        //DrehenCLinksButton.addActionListener(myController);
+        DrehenCLinksButton.addActionListener(PentagoController);
 
        
         DrehenDRechtsButton.setText("Drehen");
         DrehenDRechtsButton.setBounds(new Rectangle(750, 870, 100, 100));
         DrehenDRechtsButton.setActionCommand("DrehenDRechts");
-        //DrehenDRechtsButton.addActionListener(myController);
+        DrehenDRechtsButton.addActionListener(PentagoController);
 
         
         DrehenDLinksButton.setText("Drehen");
         DrehenDLinksButton.setBounds(new Rectangle(870, 750, 100, 100));
         DrehenDLinksButton.setActionCommand("DrehenDLinks");
-        //DrehenDLinksButton.addActionListener(myController);
+        DrehenDLinksButton.addActionListener(PentagoController);
         
         
         stopButton.setText("Stop");
         stopButton.setBounds(new Rectangle(450, 920, 100, 50));
-        //stopButton.addActionListener(myController);
+        stopButton.addActionListener(PentagoController);
+        
+        myJPanel.add(FeldAJPanel, null);
+        myJPanel.add(FeldBJPanel, null);
+        myJPanel.add(FeldCJPanel, null);
+        myJPanel.add(FeldDJPanel, null);
         
         myJPanel.add(SpielfeldJPanel, null);
+        
         
         myJPanel.add(spieler1TextField, null);
         myJPanel.add(spieler2TextField, null);
@@ -111,7 +129,7 @@ public class View  extends JFrame
         myJPanel.add(jLabel1, null);
         myJPanel.add(jLabel2, null);
         
-        myJPanel.setBackground(Color.green);
+        myJPanel.setBackground(Color.blue);
         myJPanel.add(DrehenARechtsButton, null);
         myJPanel.add(DrehenALinksButton, null);
         myJPanel.add(DrehenBRechtsButton, null);
